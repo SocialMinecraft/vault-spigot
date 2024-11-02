08b416b27ee0618bc9599dbe2fb7c3cb1ea2ffdc
Dylan Garvis <dylan@garvis.dev>
2024-11-01 21:08:43 -0400

    chore: connect to nats and include protos.

---
+++ src/main/java/club/somc/vaultSpigot/VaultSpigot.java
@@ -0,0 +1,32 @@
+package club.somc.vaultSpigot;
+
+import io.nats.client.Connection;
+import io.nats.client.Nats;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.IOException;
+
+public final class VaultSpigot extends JavaPlugin {
+
+    public Connection nc;
+
+    @Override
+    public void onEnable() {
+        super.onEnable();
+        this.saveDefaultConfig();
+
+        try {
+            this.nc = Nats.connect(getConfig().getString("natsUrl"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
