7938bb87011ec9c8fed57950074eb1d753245645
Dylan Garvis <dylan@garvis.dev>
2024-11-02 17:17:29 -0400

    chore: added release script.

---
+++ release.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+# to install the tool
+# npm i -g commit-and-tag-version
+# add --first-release if first release to not bump versions.
+commit-and-tag-version --packageFiles pom.xml --bumpFiles pom.xml
